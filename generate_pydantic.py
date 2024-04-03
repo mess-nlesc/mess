c2d5a879232e5e3279c76353448b228f2e0acd79
Faruk D. <f.diblen@esciencecenter.nl>
2024-04-03 10:25:48 +0200

    simplify the json file and render all the files

--- generate_pydantic.py
+++ generate_pydantic.py
@@ -9,17 +9,10 @@ import jinja2
 from pydantic import BaseModel, ValidationError
 
 
-class ModelParameter(BaseModel):
+class ModelParameters(BaseModel):
     """_summary_
     """
-    files: List[str]
     variables: Dict[str, List[int]]
-
-
-class ParameterList(BaseModel):
-    """_summary_
-    """
-    items: List[ModelParameter]
 
 
 # load templates
@@ -32,7 +25,7 @@ environment = jinja2.Environment(
 
 # output-related variables
 experiment_count = 0
-experiment_name = "experiment"  # output folder
+output_folder = "experiment"  # output folder
 
 
 # read the variables for rendering
@@ -40,31 +33,22 @@ with open("model_variables.json", mode="r", encoding="utf-8") as variables_entry
     model_variables = json.load(variables_entry)
 
 
-model_parameter_list = ParameterList(items=model_variables)
+model_parameter_list = ModelParameters(variables=model_variables)
 
 # validate the model
 try:
-    ParameterList.model_validate(model_parameter_list)
+    ModelParameters.model_validate(model_parameter_list)
 except ValidationError as e:
     print(e)
 
 
 # print(model_parameter_list.model_json_schema())
-# print(model_parameter_list.model_dump_json())
+print(model_parameter_list.model_dump_json())
 # print(model_parameter_list)
 # print(type(model_parameter_list.items[0]))
 # print(model_parameter_list.items[0])
 # print(model_parameter_list.items[0].variables)
 
-
-# for _entry in model_parameter_list.items:
-#     print(_entry.file)
-
-#     for _variable, _value in _entry.variables.items():
-#         print(f"    {_variable}  ->  {_value} ({type(_value).__name__})")
-#         if isinstance(_value, list):
-#             for _num in _value:
-#                 print(f"        {_num}")
 
 
 def combine_variables(input_dictionary):
@@ -74,28 +58,28 @@ def combine_variables(input_dictionary):
         yield dict(zip(input_dictionary.keys(), dict_product))
 
 
-for _entry in model_parameter_list.items:
+for parameter_grid_item in combine_variables(model_parameter_list.variables):
+    # show the parameters
+    print(f"experiment:{experiment_count}  parameters:{parameter_grid_item}")
 
-    for parameter_grid_item in combine_variables(_entry.variables):
+    # check if output(experiment) folder exists otherwise, create it
+    experiment_path = f"{output_folder}_{experiment_count}"
+    if not os.path.exists(experiment_path):
+        os.mkdir(experiment_path)
 
-        # check if output(experiment) folder exists otherwise, create it
-        experiment_path = f"{experiment_name}_{experiment_count}"
-        if not os.path.exists(experiment_path):
-            os.mkdir(experiment_path)
+    # save the parameters to a file
+    with open(f"{experiment_path}/parameters.json",  mode="w", encoding="utf-8") as parameters_file:
+        json.dump(parameter_grid_item, parameters_file)
 
-        # show the parameters
-        print(f"parameters: {parameter_grid_item}")
-
-        # save the parameters to a file
-        with open(f"{experiment_path}/parameters.json",  mode="w", encoding="utf-8") as parameters_file:
-            json.dump(parameter_grid_item, parameters_file)
-
-        for _file in _entry.files:
+    # Iterate template directory to render all the template files
+    for _file in os.listdir("template"):
+        # check if current file_path is a file
+        if os.path.isfile(os.path.join("template", _file)):
 
             save_as_name = pathlib.Path(_file).stem  # file name to save the rendered content
             output_path = f"{experiment_path}/{save_as_name}"  # path for the rendered file
-            print(f"    template file: {_file}")
-            print(f"    output file: {output_path}")
+            # print(f"    template file: {_file}")
+            # print(f"    output file: {output_path}")
 
             template = environment.get_template(_file)
 
@@ -108,4 +92,4 @@ for _entry in model_parameter_list.items:
             with open(output_path, mode="w", encoding="utf-8") as model_file:
                 model_file.write(content)
 
-        experiment_count += 1
+    experiment_count += 1
